eab5d074ca27d730254efdab71b96375901fde44
Steven Tong <steventongsf@gmail.com>
2020-01-06 21:07:30 -0800

    add method to read from path

--- src/main/java/com/sfhuskie/plugins/githooks/io/FileIO.java
+++ src/main/java/com/sfhuskie/plugins/githooks/io/FileIO.java
@@ -18,6 +18,7 @@ package com.sfhuskie.plugins.githooks.io;
  */
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -26,7 +27,25 @@ import java.util.List;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.MojoExecutionException;
-
+import java.io.BufferedReader;
+import java.io.InputStream;
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * @author Steven Tong
+ * 
+ */
 public class FileIO {
     public static String encoding = "UTF-8";
     /**
@@ -69,6 +88,11 @@ public class FileIO {
         }
         return true;
     }
+    /**
+     * @param script
+     * @return
+     * @throws IOException
+     */
     public static List<String> getInitialScriptLines(File script) throws IOException {
         List<String> lines = new ArrayList<String>();
         lines.add("#!/bin/sh");
@@ -76,10 +100,20 @@ public class FileIO {
         lines.add(getCommandToAdd(script));
         return lines;
     }
+    /**
+     * @param script
+     * @param out
+     * @throws IOException
+     */
     public static void writeInitialHooksFile(File script, File out) throws IOException {
         List<String> lines = getInitialScriptLines(script);
         FileIO.writeLines(out,  lines);
     }
+    /**
+     * @param newLine
+     * @param target
+     * @return
+     */
     public static List<String> addCommand(String newLine,List<String> target) {
         // TODO 
         if (!doesFileContainLine(target, newLine)) {
@@ -87,6 +121,11 @@ public class FileIO {
         }
         return target;
     }
+    /**
+     * @param lines
+     * @param searchString
+     * @return
+     */
     public static boolean doesFileContainLine(List<String> lines, String searchString) {
         for (String line:lines) {
             if (line.contains(searchString)) {
@@ -96,6 +135,12 @@ public class FileIO {
         return false;
  
     }
+    /**
+     * @param url
+     * @param localFile
+     * @throws MalformedURLException
+     * @throws IOException
+     */
     public static void download(String url, String localFile) throws MalformedURLException, IOException {
         final int timeout = 60000;
         FileUtils.copyURLToFile(
@@ -103,5 +148,20 @@ public class FileIO {
                 new File(localFile), 
                 timeout, 
                 timeout);
+    }
+    /**
+     * @param fileName
+     * @return
+     * @throws IOException
+     */
+    public static List<String> readFileFromPath(String fileName) throws IOException {
+        List<String> lines = new ArrayList<String>();
+        InputStream is = (InputStream) FileIO.class.getResourceAsStream("/templates/"+fileName);
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line = null;
+        while ((line = br.readLine()) != null) {
+            lines.add(line);
+        }
+        return lines;
     }
 }
